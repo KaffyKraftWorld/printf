2c046814fc496fd2dffa4343c8fa123913af2995
yomiblack <yomiblacck@gmail.com>
2022-10-17 08:02:24 +0100

    checked main

--- main.h
+++ main.h
@@ -1,9 +1,21 @@
 #ifndef MAINH
 #define MAINH
 #include <stdarg.h>
-void print_binary(unsigned int n, unsigned int *printed);
-
-
+/**
+ * struct specifier - contains specifiers for printf
+ *
+ * @width: width of field to print
+ * @precision: precision of field to print
+ * @length: modifies type length. h/l flags. 0 default, +1 per l, -1 per h
+ * @widthflag: tells whether width is set
+ * @precisionflag: tells whether precision is set
+ * @left: left justify, '-'. 0 for right, 1 for left
+ * @sign: force sign, '+'. 0 for no, 1 for yes
+ * @space: space pad left side of positive numbers if leading 0s. 0 no 1 yes
+ * @zero: zero pad numbers up to width. 0 for no, 1 for yes.
+ * @zerox: add leading 0, 0x, or 0X to o, x or X specifiers. 0 no 1 yes
+ * @specifier: the data type to print, c, s, d, x, etc.
+ */
 typedef struct specifier
 {
 	unsigned int width;
@@ -19,23 +31,16 @@ typedef struct specifier
 	char specifier;
 } specifier;
 
-/* from itox.c */
-char *itox(va_list list);
-char *litox(va_list list);
-char *hitox(va_list list);
-char *hhitox(va_list list);
+/* from printf.c */
+int _printf(char *format, ...);
 
-/* from itox.c */
-char *itoX(va_list list);
-char *hitoX(va_list list);
-char *hhitoX(va_list list);
-char *litoX(va_list list);
+/* from prep_types.c */
+char *prep_string(char *str, specifier spec);
+char *prep_numeric(char *str, specifier spec);
 
-/* from itoo.c */
-char *itoo(va_list list);
-char *litoo(va_list list);
-char *hitoo(va_list list);
-char *hhitoo(va_list list);
+/* from utility.c */
+int _strlen(char *str);
+int ifputs(char *s);
 
 /* from itos.c */
 char *itos(va_list list);
@@ -49,5 +54,37 @@ char *luitob(va_list list);
 char *huitob(va_list list);
 char *hhuitob(va_list list);
 
-#endif
+/* from utos.c */
+char *utos(va_list list);
+char *hutos(va_list list);
+char *hhutos(va_list list);
+char *lutos(va_list list);
 
+/* from itoo.c */
+char *itoo(va_list list);
+char *litoo(va_list list);
+char *hitoo(va_list list);
+char *hhitoo(va_list list);
+
+/* from itox.c */
+char *itox(va_list list);
+char *litox(va_list list);
+char *hitox(va_list list);
+char *hhitox(va_list list);
+
+/* from itox.c */
+char *itoX(va_list list);
+char *hitoX(va_list list);
+char *hhitoX(va_list list);
+char *litoX(va_list list);
+
+/* from stos.c */
+char *rev(char *str);
+char *rot(char *str);
+char *print_hidden(char *str);
+char *null(void);
+
+/* from ptos.c */
+char *nil(void);
+
+#endif
